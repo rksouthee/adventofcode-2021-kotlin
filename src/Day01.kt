e109a1b0c7885b2183f8753e82fc75d32d5dd97f
rksouthee <rksouthee@gmail.com>
2021-12-26 12:59:28 +0000

    Add check for day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -20,6 +20,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test").map { it.toInt() }
     check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01").map{ it.toInt() }
     println(part1(input))
